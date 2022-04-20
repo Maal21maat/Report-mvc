486571acdee471124dbc9e2700bec03f3c9d30af
Maal21maat <maat21@student.bth.se>
2022-04-20 17:03:00 +0100

    felsökning för publicering

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+/** use Symfony\Component\HttpKernel\Exception\NotFoundHttpException; */
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReportController extends AbstractController
